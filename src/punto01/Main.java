6fa17ae0a758f095f78008a16bbdd847739eb2f7
Carolina Tapia <caro18tapia@gmail.com>
2025-09-03 22:14:52 -0300

    punto1

--- src/punto01/Main.java
+++ src/punto01/Main.java
@@ -1,5 +1,26 @@
 package punto01;
 
 public class Main {
+     public static void main(String[] args) throws Exception {
+
+    
+        String pais= "Argentina";
+        int edad = 18;
+        double altura = 150.50;
+        double precio = 1999.99;
+        long telefono = 3884482473L;
+        double coseno = Math.cos(0.5);
+        int n1 = 5, n2 = 10, n3 = 15, n4= 20, n5 = 25;
+        double promedio = (n1 + n2 + n3 + n4 + n5) / 5.0;
+
+        System.out.println("Pais: " + pais); 
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura de edificio: " + altura);
+        System.out.println("Precio del producto: " + precio);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Coseno de 0.5: " + coseno);
+        System.out.println("Promedio: " + promedio);
+     }
+        
 
 }
